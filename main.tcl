24099488c065a148a0ebff91f365cb743ed583e0
Alireza <alireza.zarenejad99@gmail.com>
2020-06-26 14:58:37 +0430

    last

--- main.tcl
+++ main.tcl
@@ -172,18 +172,13 @@ source TraceApp.ns
 
 proc plotThroughput {tcpSink outfile} {
    global ns
-
    set now [$ns now]
    set nbytes [$tcpSink set bytes_]
    $tcpSink set bytes_ 0
-
    set time_incr 1.0
-
    set throughput [expr ($nbytes * 8.0 / 1000000) / $time_incr]
-
 ###Print TIME throughput for  gnuplot to plot progressing on throughput
    puts  $outfile  "$now $throughput"
-
    $ns at [expr $now+$time_incr] "plotThroughput $tcpSink  $outfile"
 }
 
